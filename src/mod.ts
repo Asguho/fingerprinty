7937764b93aa4be442776aa40ce2301c8d707387
Asguho <78410939+Asguho@users.noreply.github.com>
2025-01-21 20:49:02 +0100

    trying to publish to jsr

--- src/mod.ts
+++ src/mod.ts
@@ -2,11 +2,10 @@ import { hash } from './utils.ts';
 import { getNavigatorData } from './datapoints/navigator.ts';
 import { getScreenData } from './datapoints/screen.ts';
 
-export function getId() {
+export function getId(): number {
   return hash(JSON.stringify(getFingerprint()));
 }
-
-export function getFingerprint() {
+export function getFingerprint(): { [key: string]: any } {
   return {
     navigator: getNavigatorData(),
     screen: getScreenData(),
