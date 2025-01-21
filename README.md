d916ecec0f20c1da0be6e151e588bf668359d6ef
Asguho <78410939+Asguho@users.noreply.github.com>
2025-01-21 21:08:02 +0100

    added example and documentation

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Fingerprinty
 A basic fingerprinting library.
+## Example
+### Browser
+```js
+import { getId } from 'https://esm.sh/jsr/asguho/fingerprinty';
+console.log(await getId());
+```
+### Node
+```bash
+npx jsr add asguho/fingerprinty
+```
+```js
+import { getId } from 'fingerprinty';
+console.log(await getId());
+```
+
 ## Credit
 These have all been a mayor help developing this library
 [ThumbmarkJs](https://www.thumbmarkjs.com/)
--- src/mod.ts
+++ src/mod.ts
@@ -1,10 +1,15 @@
 import { hash } from './utils.ts';
 import { getNavigatorData } from './datapoints/navigator.ts';
 import { getScreenData } from './datapoints/screen.ts';
-
+/**
+ * Retrieves a unique identifier based on the fingerprint object.
+ */
 export function getId(): number {
   return hash(JSON.stringify(getFingerprint()));
 }
+/**
+ * Retrieves a fingerprint object containing various data points.
+ */
 export function getFingerprint(): { [key: string]: any } {
   return {
     navigator: getNavigatorData(),
